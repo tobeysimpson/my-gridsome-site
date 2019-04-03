a1df14e13adafe7797e96aab4c24fdc8e8b87d68
tobeysimpson <32686225+tobeysimpson@users.noreply.github.com>
2019-04-02 21:06:18 -0500

    Create about.vue

---
+++ src/pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    Some content about us...
+  </Layout>
+</template>
+
+<script>
+import Layout from '~/layouts/Default.vue'
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
