f6a6ef823d1e1810ba7ace10836ec52b41decd0c
tobeysimpson <32686225+tobeysimpson@users.noreply.github.com>
2019-04-25 16:03:56 -0500

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,7 +17,7 @@
 
     <footer class="footer">
       <span class="footer__copyright">Copyright © {{ new Date().getFullYear() }}. </span>
-      <span class="footer__links">Powered by <a href="//gridsome.org"> EORROE </a></span>
+      <span class="footer__links">Powered by <a href="//gridsome.org"> CRAZY </a></span>
     </footer>
 
   </div>
